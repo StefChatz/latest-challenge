da2735adbc156499f3cb47417321585783b4c99a
dimtirisafendras <dimitris.afendras@gmail.com>
2021-01-31 12:32:06 +0200

    styling feedback card

--- src/components/ratingCard/RatingCard.js
+++ src/components/ratingCard/RatingCard.js
@@ -1,11 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./RatingCard.css";
 
 export const RatingCard = ({ rating: { rating, comment } }) => (
   <div className="RatingCard">
-    Thank you for your feedback
-    <div>{rating}</div>
-    <div>{comment}</div>
+    <div className="RatingCard__title">Thank you for your feedback!</div>
+    <div className="RatingCard__rating">{rating}</div>
+    <div className="RatingCard__comment">{comment}</div>
   </div>
 );
 
